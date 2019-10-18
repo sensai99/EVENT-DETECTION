059a82e22d38744e14c2d59b34219321fe811ca6
Sreenivas-2 <52418873+Sreenivas-2@users.noreply.github.com>
2019-10-18 23:29:07 +0530

    Update twitter_credentials.py

--- CODE/DATA_EXTRACTION/GetLatestTweets/twitter_credentials.py
+++ CODE/DATA_EXTRACTION/GetLatestTweets/twitter_credentials.py
@@ -1,6 +1,5 @@
-# YouTube Video: https://www.youtube.com/watch?v=wlnx-7cm4Gg
 # Variables that contains the user credentials to access Twitter API
-ACCESS_TOKEN = "1175631292236320768-EnyvkEQQMUsaUTLiVOqacWa8H3xW3T"
-ACCESS_TOKEN_SECRET = "r6JQRZkZ4s0iUbFY4SrlGyb423kxOBYDOQDfNL615WciN"
-CONSUMER_KEY = "lV5XSQKJc5bbdwekQuCoqhYy9"
-CONSUMER_SECRET = "oR0Nl3jvoZLHZ7mGxVOwYAMhstDewpp2Of97lm6nomHaEBvZXn"
+ACCESS_TOKEN = ""
+ACCESS_TOKEN_SECRET = ""
+CONSUMER_KEY = ""
+CONSUMER_SECRET = ""
