b752d8cbfc3db55cc306d771e9d381247a8655d2
Sai Sreenivas <52418873+Sreenivas-2@users.noreply.github.com>
2020-05-07 22:08:40 +0530

    Add files via upload

--- PROJECT_EMAIL/CODE/ALGORITHM/count_min_sketch.py
+++ PROJECT_EMAIL/CODE/ALGORITHM/count_min_sketch.py
@@ -1,5 +1,6 @@
 import numpy as np
 import mmh3
+import sys
 
 class CountMinSketch(object):
     ''' Class for a CountMinSketch data structure
@@ -29,47 +30,22 @@ class CountMinSketch(object):
         '''
         for i in range(0, self.width):
             index = mmh3.hash(key, self.seed[i]) % self.length
-            print(index)
             self.table[i, index] = self.table[i, index]+1
-
-    def get_hashindices(self, key) :
-    	''' 
-    	This method returns the indices of a key in Sktech_Table
-        '''
-	index_list = []
-    	for i in range(0, self.width):
-    		index = mmh3.hash(key, self.seed[i]) % self.length
-    		index_list.append(index)
-
-    	return index_list
 
     def estimate(self, key):
         ''' 
-        This method estimates if the key is in Sketch_Table
+        This method returns the frequency of a key in the Sketch_Table
         @param key str: A string to check
         '''
-        min_est = self.width
+        min_est = sys.maxsize
         for i in range(0, self.width):
             index = mmh3.hash(key, self.seed[i]) % self.length
             if self.table[i, index] < min_est:
                 min_est = self.table[i, index]
         return min_est
 
-    # def merge(self, new_cms):
-    #     ''' Method to combine two count min sketches
-    #     @param new_cms CountMinSketch: Another CMS object
-    #     '''
-    #     return self.table + new_cms
-
-
-cms = CountMinSketch(2, 16369, [3,1])
-cms.increment("sai")
-cms.increment("sai")
-cms.increment("sree")
-cms.increment("sreee")
-list_ = cms.get_hashvalues("sai")
-print(list_)
-table = cms.sketch_table()
-print(table[0, list_[0]])
-print(table[1, list_[1]])
-print(cms.estimate("sre"))
+    def get_total_frequency(self):
+        '''
+        This method returns the sum of all the elements in a row.
+        '''
+    	return np.sum(self.table[0])
